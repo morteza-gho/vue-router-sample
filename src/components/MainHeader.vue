d25533221098eac7357e8f559f645f5e7f149784
Morteza QorbanAlizade <gho.morteza@gmail.com>
2023-01-12 11:48:04 +0330

    some changes

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,10 +1,10 @@
 <template>
   <header id="main-header">
     <ul>
-      <li><router-link to="/" active-class="active">Home</router-link></li>
-      <li><router-link to="/contact" active-class="active">Contact us</router-link></li>
-      <li><router-link to="/about" active-class="active">About us</router-link></li>
-      <li><router-link :to="{name: 'posts'}" active-class="active">Posts</router-link></li>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/contact">Contact us</router-link></li>
+      <li><router-link to="/about">About us</router-link></li>
+      <li><router-link :to="{ name: 'posts' }">Posts</router-link></li>
     </ul>
   </header>
 </template>
@@ -14,12 +14,12 @@ export default {
 }
 </script>
 <style scoped>
-
 #main-header {
   background: #EEE;
   padding: 10px 30px;
   border-bottom: 1px solid #CCC;
 }
+
 ul {
   padding: 0;
   display: flex;
@@ -29,15 +29,20 @@ ul li {
   list-style-type: none;
   margin-right: 30px;
 }
+
 ul li a {
   text-decoration: none;
   color: #333;
-  font-size: 18px;  
+  font-size: 18px;
 }
+
 ul li a:hover {
   color: red;
 }
-.router-link-active, .router-link-exact-active, .active {
+
+.router-link-active,
+.router-link-exact-active,
+.active {
   color: red;
 }
 </style>
